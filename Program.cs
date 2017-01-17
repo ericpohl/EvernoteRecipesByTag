bf1e6b3ef2a2d4102636282ef7c52d1fc8f69e99
Pohl, Eric <Eric.Pohl@standardregister.com>
2017-01-17 12:42:45 -0500

    Listing notebooks via simple interface

--- Program.cs
+++ Program.cs
@@ -4,12 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using EvernoteSDK;
+
 namespace EvernoteRecipesByTag
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string developerToken;
+            if (args.Length > 0)
+            {
+                developerToken = args[0];
+            }
+            else
+            {
+                Console.WriteLine("Enter developer token:");
+                developerToken = Console.ReadLine();
+            }
+
+            string notestoreUrl;
+            if (args.Length > 1)
+            {
+                notestoreUrl = args[1];
+            }
+            else
+            {
+                Console.WriteLine("Enter note store URL:");
+                notestoreUrl = Console.ReadLine();
+            }
+
+            ENSession.SetSharedSessionDeveloperToken(
+                developerToken,
+                notestoreUrl);
+
+            var session = ENSession.SharedSession;
+            foreach (var notebook in session.ListNotebooks())
+            {
+                Console.WriteLine(notebook.Name);
+            }
         }
     }
 }
